ed96cd21c02b145c0b6fa05fb3a6014e51730dd4
Milena Krawczyk <mmilena.krawczyk@gmail.com>
2023-09-28 14:31:52 +0200

    Request body input validation

---
+++ src/pipes/JoiValidationPipe.ts
@@ -0,0 +1,23 @@
+import {
+  PipeTransform,
+  Injectable,
+  ArgumentMetadata,
+  BadRequestException,
+} from '@nestjs/common';
+import { Schema } from 'joi';
+
+@Injectable()
+export class JoiValidationPipe implements PipeTransform {
+  constructor(private schema: Schema) {}
+
+  transform(value: any, _metadata: ArgumentMetadata) {
+    const { error } = this.schema.validate(value);
+
+    if (error) {
+      throw new BadRequestException(
+        error?.details?.[0]?.message || 'Validation failed',
+      );
+    }
+    return value;
+  }
+}
---
+++ src/subscriptions/dto.ts
@@ -0,0 +1,36 @@
+import * as Joi from 'joi';
+
+// create subscription
+export interface PushSubscriptionObjectDto {
+  endpoint: string;
+  expirationTime?: Number;
+  keys: {
+    p256dh: string;
+    auth: string;
+  };
+}
+export const PushSubscriptionObjectSchema = Joi.object({
+  endpoint: Joi.string().uri().required(),
+  expirationTime: Joi.number().allow(null),
+  keys: Joi.object({
+    p256dh: Joi.string().required(),
+    auth: Joi.string().required(),
+  }).required(),
+});
+
+export interface CreateSubscriptionDto {
+  accountId: string;
+  subscription: PushSubscriptionObjectDto;
+}
+export const CreateSubscriptionSchema = Joi.object({
+  accountId: Joi.string().min(2).max(64).required(),
+  subscription: PushSubscriptionObjectSchema,
+});
+
+// delete subscription
+export interface DeleteSubscriptionDto {
+  endpoint: string;
+}
+export const DeleteSubscriptionSchema = Joi.object({
+  endpoint: Joi.string().uri().required(),
+});
